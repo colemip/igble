85fb041364d8af4eb86d6083285e417bcb1b315c
Ian Coleman <colemip@gmail.com>
2014-03-22 17:41:47 -0400

    initalize script on page load

--- js/drag_drop.js
+++ js/drag_drop.js
@@ -1,9 +1,25 @@
-if(!window.drag_drop) { window.drag_drop = {}; }
+if(!window.igble) { window.igble = {}; }
+if(!window.igble.proj) { window.igble.proj = {}; }
+if(!window.igble.proj.dragDrop) { window.igble.proj.dragDrop = {}; }
 
-igble.proj.drag_drop = {
+igble.proj.dragDrop = {
 	init: function() {
+		$('.igble-proj-draggable').draggable();
+	},
+	
+	onDrag: function(element) {
 		
 	},
 	
+	onDrop: function(element) {
+		
+	}
+	
+	
 	
 };
+
+$(document).ready(function() {
+	console.log("page ready");
+	igble.proj.dragDrop.init();
+});
