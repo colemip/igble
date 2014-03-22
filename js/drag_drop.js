2a99986129e0a7ec51b1de7158b8ea2011b2b4e2
Ian Coleman <colemip@gmail.com>
2014-03-22 18:32:57 -0400

    Setting up test drag/drops, adding functions to make sentence elements draggable

--- js/drag_drop.js
+++ js/drag_drop.js
@@ -4,7 +4,17 @@ if(!window.igble.proj.dragDrop) { window.igble.proj.dragDrop = {}; }
 
 igble.proj.dragDrop = {
 	init: function() {
-		$('.igble-proj-draggable').draggable();
+		igble.proj.dragDrop.splitIntoDraggables("This is a sentence");
+		
+		$('.igble-proj-draggable').draggable({
+			revert: true
+		});
+		$('.igble-proj-droppable').droppable({
+			accept: '.igble-proj-draggable', 
+			activate: 
+				function(event, ui) {},
+						
+		});
 	},
 	
 	onDrag: function(element) {
@@ -13,9 +23,24 @@ igble.proj.dragDrop = {
 	
 	onDrop: function(element) {
 		
+	},
+	
+	makeDraggable: function(element) {
+		console.log(element);
+		$(element).draggable();		
+	},
+	
+	splitIntoDraggables: function(string) {
+		var words = string.split(' ');
+		console.log(words);
+		for(var i=0; i < words.length; i++) {
+			var word = words[i];
+			var $draggableWord = $("<div class='.igble-proj-draggable'></div>");
+			$draggableWord.append(word);
+			igble.proj.dragDrop.makeDraggable($draggableWord);
+			$('main').append($draggableWord);
+		}
 	}
-	
-	
 	
 };
 
