7f0688bbd1f5bcd63ff74337bafa4d3d104b34f6
Ian Coleman <colemip@gmail.com>
2014-04-08 20:23:49 -0400

    [#13] Defining onDrop() for verb-adverbs

--- js/drag_drop.js
+++ js/drag_drop.js
@@ -106,6 +106,12 @@ igble.proj = {
 	
 	_onDropAdverb: function(element) {
 		console.log('adverb dropped!');
+		$(element).parent('.adverb-group').append("<span class='droppable-token adverb' data-pos='adv'></span>");
+		$(element).append("<div class='adverb-group'></div>");
+		$(element).find('.adverb-group').append("<div class='droppable-token adverb dropped'></div>");
+		igble.proj.dragDrop.makeDroppable($(element).find('.adverb.dropped'));									
+		$(element).css('top', '-2.0em');
+		$(element).rotate({angle: 30, center: ["0%", "0%"]});
 	},
 	
 	
