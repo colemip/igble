b821ed63f4cfc7a1a95b415b018944332b4732c3
Ian Coleman <colemip@gmail.com>
2014-04-12 19:03:09 -0400

    Setting up player score display

--- js/assessment.js
+++ js/assessment.js
@@ -4,6 +4,11 @@ if(!window.igble.proj.assessment) { window.igble.proj.assessment = {}; }
 
 igble.proj = {	
 	assessment : {
+		player_score: 0,
+		
+		getPlayerScore: function() {
+			return igble.proj.assessment.player_score;
+		}
 		
 	}
 };
