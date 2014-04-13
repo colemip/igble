37a2a3d9bfea0e1d4f6d1c81095ecb6a1d44f534
Ian Coleman <colemip@gmail.com>
2014-04-13 12:13:20 -0400

    [#14] Setting up gameplay timer

--- js/assessment.js
+++ js/assessment.js
@@ -5,6 +5,11 @@ if(!window.igble.proj.assessment) { window.igble.proj.assessment = {}; }
 igble.proj.assessment = {	
 	
 		player_score: 0,
+		timer_value: 0,
+		
+		init: function() {
+			igble.proj.assessment.updateTimer();
+		},
 		
 		getPlayerScore: function() {
 			return igble.proj.assessment.player_score;
@@ -13,7 +18,17 @@ igble.proj.assessment = {
 		updatePlayerScore: function(amount) {
 			this.player_score += amount;
 			$('#score').text(this.getPlayerScore());
-		}
+		},
 		
-	
+		updateTimer: function() {
+			igble.proj.assessment.timer_value++;
+			if(igble.proj.assessment > 10) return;
+			$('#timer').text(igble.proj.assessment.timer_value);		
+			setTimeout(igble.proj.assessment.updateTimer, 1000);
+			
+		}			
 };
+
+$(document).ready(function() {
+	igble.proj.assessment.init();
+});
