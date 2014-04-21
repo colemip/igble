7a193f5f4c485a585d01f4639422c1ba773adea0
Ian Coleman <colemip@gmail.com>
2014-04-21 14:52:53 -0400

    Missing semicolon

--- js/drag_drop.js
+++ js/drag_drop.js
@@ -363,7 +363,7 @@ igble.proj.dragDrop = {
 	addToTokenBank: function(tokens) {
 		var $tokenBank = $('#token-bank');
 		$(tokens).each(function(key, value) {
-			$tokenBank.append("<span class='draggable-token token rounded " + value.pos + "' data-role='" + value.role + "' data-pos='" + value.pos + "'>" + value.text + "</span>")
+			$tokenBank.append("<span class='draggable-token token rounded " + value.pos + "' data-role='" + value.role + "' data-pos='" + value.pos + "'>" + value.text + "</span>");
 			$tokenBank.children().last().on('click', function() {
 							console.log("token bank token clicked");
 							console.log($(this));
