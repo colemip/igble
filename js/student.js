a056fb3e26e9eb6c07db560dc5ec0dbea4df4d47
Ian Coleman <colemip@gmail.com>
2014-04-22 07:10:40 -0400

    Adding file for student modeling

---
+++ js/student.js
@@ -0,0 +1,13 @@
+if(!window.igble) { window.igble = {}; }
+if(!window.igble.proj) { window.igble.proj = {}; }
+if(!window.igble.proj.dragDrop) { window.igble.proj.student = {}; }
+
+igble.proj.student = {	
+	errors: {
+		'subject': 0,
+		'verb': 0,
+		'noun': 0,
+		'adj': 0,
+		'adv': 0
+	}
+};
